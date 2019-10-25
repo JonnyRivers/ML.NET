aada0a52a13aabec6400fba7dcac08862a01b720
JonnyRivers <jonny.w.rivers@gmail.com>
2019-10-24 21:28:09 -0700

    Added a couple of sample apps

---
+++ TaxiFare/TaxiFare/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using TaxiFareML.Model;
+
+namespace TaxiFare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput
+            {
+                Vendor_id = "CMT",
+                Rate_code = 1,
+                Passenger_count = 1,
+                Trip_time_in_secs = 20 * 60,
+                Trip_distance = 12,
+                Payment_type = "CRD"
+            };
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+
+            Console.WriteLine($"prediction: {result.Score};");
+        }
+    }
+}
---
+++ ToxOrNot/MyFirstMLApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using MyFirstMLAppML.Model;
+
+namespace MyFirstMLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput
+            {
+                SentimentText = "My name is Michael Phelps."
+            };
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+
+            Console.WriteLine($"prediction: {result.Prediction}; score: {result.Score};");
+        }
+    }
+}
